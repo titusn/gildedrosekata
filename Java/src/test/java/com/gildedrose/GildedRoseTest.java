23a16598cb34b0696d5efee2ef50782f765199c7
titusn <mail@titusnachbauer.com>
2021-02-07 10:21:30 +0100

    Reformat code

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,14 +12,14 @@ class GildedRoseTest {
     void updateQualityTest() {
         CombinationApprovals.verifyAllCombinations(
                 this::doUpdateQuality,
-                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"},
+                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
                 new Integer[]{-1, 0, 5, 6, 11},
                 new Integer[]{0, 1, 50, 49}
         );
     }
 
     private String doUpdateQuality(String name, int sellIn, int quality) {
-        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         String itemString = app.items[0].toString();
