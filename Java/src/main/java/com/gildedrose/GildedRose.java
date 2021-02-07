55761a477066f86abce1dce562f0d3c04e0591f6
titusn <mail@titusnachbauer.com>
2021-02-07 10:55:04 +0100

    Clean up if statements

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -52,15 +52,6 @@ class GildedRose {
                 }
             } else {
                 if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.sellIn = item.sellIn - 1;
-                    }
 
                     if (item.sellIn < 0) {
                         if (item.quality > 0) {
