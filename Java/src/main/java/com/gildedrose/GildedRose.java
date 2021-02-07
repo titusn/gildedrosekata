221fc2120d6020b5f1eb3ba24d9c4b61b0c7f490
titusn <mail@titusnachbauer.com>
2021-02-07 10:57:07 +0100

    Clean up if statement

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -62,9 +62,7 @@ class GildedRose {
 
                     if (item.sellIn < 0) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
+                            item.quality = item.quality - 1;
                         }
                     }
                 }
