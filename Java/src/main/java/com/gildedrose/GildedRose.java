7c2d61249531c6653ef0ec9e548f01d5a5c08e82
titusn <mail@titusnachbauer.com>
2021-02-07 10:40:48 +0100

    Remove if statement

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -48,15 +48,7 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
+                    item.quality = 0;
                 }
             } else {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
