bfdd90cadf7f1c4e2ba8da137facbf5dc5300683
titusn <mail@titusnachbauer.com>
2021-02-07 11:17:48 +0100

    Add BackstagePass subclass

---
+++ Java/src/main/java/com/gildedrose/BackstagePass.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class BackstagePass extends Item {
+    public BackstagePass(int sellIn, int quality) {
+        super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+}
