ceda72c61d4d2c8e4c4f99f2dc7fb3518c101d94
titusn <mail@titusnachbauer.com>
2021-02-09 20:31:12 +0100

    Add Conjured item tests to combination approvals

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,27 +12,15 @@ class GildedRoseTest {
     void updateQualityTest() {
         CombinationApprovals.verifyAllCombinations(
                 this::doUpdateQuality,
-                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new String[]{
+                        "foo",
+                        "Aged Brie",
+                        "Backstage passes to a TAFKAL80ETC concert",
+                        "Sulfuras, Hand of Ragnaros",
+                        "Conjured"},
                 new Integer[]{-1, 0, 5, 6, 11},
                 new Integer[]{0, 1, 50, 49}
         );
-    }
-
-    @Test
-    void createConjuredItem() {
-        Item conjuredItem = new Item("Conjured", 1, 2);
-        assertNotNull(conjuredItem);
-        assertTrue(conjuredItem.toString().startsWith("Conjured"));
-        assertEquals(conjuredItem.sellIn, 1);
-        assertEquals(conjuredItem.quality, 2);
-    }
-
-    @Test
-    void createConjuredItemQualityShouldDegradeTwiceAsFast() {
-        Item conjuredItem = Item.createItem("Conjured", 1, 2);
-        conjuredItem.updateQuality();
-        assertEquals(0, conjuredItem.sellIn, "SellIn should be lowered by 1");
-        assertEquals(0, conjuredItem.quality, "Quality should degrade twice as fast as normal item");
     }
 
     private String doUpdateQuality(String name, int sellIn, int quality) {
